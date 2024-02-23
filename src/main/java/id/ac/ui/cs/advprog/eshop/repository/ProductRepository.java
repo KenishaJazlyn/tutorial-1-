b2043705dec5e2194199d418684b02847a071da8
KenishaJazlyn <kenishajazlyn@gmail.com>
2024-02-24 01:27:23 +0700

    Implement DIP: depend on abstractions instead of concreate implementation

--- src/main/java/id/ac/ui/cs/advprog/eshop/repository/ProductRepository.java
+++ src/main/java/id/ac/ui/cs/advprog/eshop/repository/ProductRepository.java
@@ -2,6 +2,7 @@ package id.ac.ui.cs.advprog.eshop.repository;
 
 import id.ac.ui.cs.advprog.eshop.model.Product;
 import org.springframework.stereotype.Repository;
+import id.ac.ui.cs.advprog.eshop.repository.ProductRepositoryInterface;
 
 import java.util.ArrayList;
 import java.util.Iterator;
@@ -9,9 +10,10 @@ import java.util.List;
 import java.util.UUID;
 
 @Repository
-public class ProductRepository {
+public class ProductRepository implements ProductRepositoryInterface {
     private List<Product> productData = new ArrayList<>();
 
+    @Override
     public Product create(Product product) {
         if (product.getProductId() == null){
             UUID uuid = UUID.randomUUID();
@@ -20,13 +22,15 @@ public class ProductRepository {
         productData.add(product);
         return product;
     }
-
+    @Override
     public Iterator<Product> findAll() {
         return productData.iterator();
     }
+    @Override
     public void delete(Product product) {
         productData.remove(product);
     }
+    @Override
     public void updateProduct(Product updatedProduct) {
         for (Product product : productData) {
             if (product.getProductId().equals(updatedProduct.getProductId())) {
