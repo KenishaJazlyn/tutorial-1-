8dd81a5ba3b63b023d308d3b554cec7178c3323e
KenishaJazlyn <kenishajazlyn@gmail.com>
2024-03-05 16:44:24 +0700

    [GREEN] Implement Payment model

--- src/main/java/id/ac/ui/cs/advprog/eshop/model/Payment.java
+++ src/main/java/id/ac/ui/cs/advprog/eshop/model/Payment.java
@@ -7,7 +7,6 @@ import java.util.Map;
 import id.ac.ui.cs.advprog.eshop.enums.OrderStatus;
 import lombok.Builder;
 import lombok.Getter;
-import lombok.Setter;
 
 @Builder
 @Getter
@@ -16,13 +15,38 @@ public class Payment {
     String method;
     Map<String, String> paymentData;
     Order order;
-
-    @Setter
     String status;
     
     public Payment(String id, String method, Map<String, String> paymentData, Order order){
+        this.id = id;
+        this.method = method;
+        this.status = null;
+        
+        if (paymentData == null || paymentData.isEmpty()){
+            throw new IllegalArgumentException("PaymentData is Empty or null");
+        } else {
+            this.paymentData = paymentData;
+        }
+
+        if (order == null) {
+            throw new IllegalArgumentException("Order must have at least one product");
+        } else {
+            this.order = order;
+        }
     }
 
     public Payment(String id, String method, Map<String, String> paymentData, Order order, String status){
+        this(id, method, paymentData, order);
+        this.setStatus(status);
     }
+
+    public void setStatus(String status) {
+        String[] statusList = {"SUCCESS", "REJECTED"};
+        if (Arrays.stream(statusList).noneMatch(item -> item.equals(status))) {
+            throw new IllegalArgumentException();
+        } else {
+            this.status = status;
+        }
+    }
+
 }
