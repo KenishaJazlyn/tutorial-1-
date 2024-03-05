412ebaeff96342d3a9f9903a7a72d546da75570a
KenishaJazlyn <kenishajazlyn@gmail.com>
2024-03-05 16:53:32 +0700

    [REFACTOR] Apply PaymentMethod enums

--- src/main/java/id/ac/ui/cs/advprog/eshop/model/Payment.java
+++ src/main/java/id/ac/ui/cs/advprog/eshop/model/Payment.java
@@ -4,6 +4,7 @@ import java.util.Arrays;
 import java.util.List;
 import java.util.Map;
 
+import id.ac.ui.cs.advprog.eshop.enums.PaymentMethod;
 import lombok.Builder;
 import lombok.Getter;
 
@@ -18,7 +19,7 @@ public class Payment {
     
     public Payment(String id, String method, Map<String, String> paymentData, Order order){
         this.id = id;
-        this.method = method;
+        this.setMethod(method);
         this.status = null;
         
         if (paymentData == null || paymentData.isEmpty()){
@@ -45,6 +46,13 @@ public class Payment {
             throw new IllegalArgumentException();
         } else {
             this.status = status;
+        }
+    }
+    public void setMethod(String method) {
+        if (PaymentMethod.contains(method)) {
+            this.method = method;
+        } else {
+            throw new IllegalArgumentException();
         }
     }
 
